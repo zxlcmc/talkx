e0ef1e6fe4a9d884eb2ea71fc6cb54e0dc1c840e
Allen Hu <allen.hu@bianxianmao.com>
2025-03-21 17:41:25 +0800

    refactor(xiaozhi):重构 Xiaozhi TTS 服务
    - 移除 TalkXApi 依赖，直接与 TalkX Center 交互
    - 使用 OkHttpClient 替代 forest 进行 HTTP 请求
    -优化 TTS 流式处理逻辑

--- src/main/java/org/bigmouth/gpt/xiaozhi/tts/TalkXTtsService.java
+++ src/main/java/org/bigmouth/gpt/xiaozhi/tts/TalkXTtsService.java
@@ -1,9 +1,16 @@
 package org.bigmouth.gpt.xiaozhi.tts;
 
+import com.bxm.warcar.utils.JsonHelper;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.bigmouth.gpt.xiaozhi.forest.TalkXApi;
-import org.bigmouth.gpt.xiaozhi.forest.TtsRequest;
+import okhttp3.*;
+import org.apache.commons.io.IOUtils;
+import org.bigmouth.gpt.xiaozhi.config.XiaozhiTalkXConfig;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 /**
@@ -13,10 +20,23 @@ import java.util.function.Consumer;
 @Slf4j
 public class TalkXTtsService implements TtsService {
 
-    private final TalkXApi talkXApi;
+    private final String talkxCenterBaseUrl;
+    private final OkHttpClient okHttpClient = create();
+    private final XiaozhiTalkXConfig xiaozhiTalkXConfig;
+    private final TalkXTtsService.TtsConfig ttsConfig;
 
-    public TalkXTtsService(TalkXApi talkXApi) {
-        this.talkXApi = talkXApi;
+    public TalkXTtsService(String talkxCenterBaseUrl, XiaozhiTalkXConfig xiaozhiTalkXConfig, TtsConfig ttsConfig) {
+        this.talkxCenterBaseUrl = talkxCenterBaseUrl;
+        this.xiaozhiTalkXConfig = xiaozhiTalkXConfig;
+        this.ttsConfig = ttsConfig;
+    }
+
+    private OkHttpClient create() {
+        return new OkHttpClient.Builder()
+                .connectionPool(new ConnectionPool(100, 10, TimeUnit.MINUTES))
+                .connectTimeout(Duration.ofSeconds(3))
+                .readTimeout(Duration.ofMinutes(10))
+                .build();
     }
 
     @Override
@@ -26,19 +46,60 @@ public class TalkXTtsService implements TtsService {
 
     @Override
     public void stream(String text, Consumer<byte[]> frameHandler) {
-        TtsRequest request = new TtsRequest();
-        request.setText(text);
-        byte[] bytes = talkXApi.tts(request);
-        frameHandler.accept(bytes);
+        Response response = null;
+        try {
+            ttsConfig.setText(text);
+
+            Request request = new Request.Builder()
+                    .url(talkxCenterBaseUrl)
+                    .post(RequestBody.create(JsonHelper.convert(ttsConfig), MediaType.get("application/json")))
+                    .build();
+
+            response = okHttpClient.newCall(request).execute();
+            if (response.isSuccessful() && response.body() != null) {
+                InputStream inputStream = response.body().byteStream();
+                byte[] buffer = new byte[xiaozhiTalkXConfig.getTtsStreamBufferSize()];
+                while (inputStream.read(buffer) != -1) {
+                    frameHandler.accept(buffer);
+                }
+            }
+        } catch (IOException e) {
+            log.error("Error occurred while processing the request: " + e.getMessage());
+        } finally {
+            IOUtils.closeQuietly(response);
+        }
     }
 
     @Override
     public void init() {
-
     }
 
     @Override
     public void destroy() {
+    }
 
+    @Data
+    public static class TtsConfig {
+        /**
+         * sessionId
+         */
+        private String sessionId;
+        /**
+         * 平台
+         */
+        private TtsPlatformType ttsPlatformType;
+        /**
+         * 合成大模型
+         */
+        private String voiceModel;
+        /**
+         * 音色角色
+         */
+        private String voiceRole;
+
+        /**
+         * 说话内容
+         */
+        private String text;
     }
 }
